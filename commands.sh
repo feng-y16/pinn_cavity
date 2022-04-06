df34442b4c061d983ff8ea6d7e536c3ca2504898
Yao Feng <v-yaofeng@microsoft.com>
2022-04-06 05:45:51 +0000

    add random data generation

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 # ground truth
 python main.py -n pinn -l l2 -i 10000 -ntr 20000 -gi 100
 # ag1
-python main.py -n pinn -l ag -i 10 -ntr 200 -gi 2000
+python main.py -n pinn -l ag -i 10000 -ntr 200 -gi 2000
 # pinn1
 python main.py -n pinn -l l2 -i 10000 -ntr 3200 -gi 100
 # ag2
@@ -11,4 +11,4 @@ python main.py -n gfnn -l l2 -i 10000 -ntr 400 -gi 100
 # just for small testing
 python main.py -n pinn -l l2 -i 10 -ntr 10 -gi 100
 # all
-python main.py -n pinn -l l2 -i 10000 -ntr 20000 -gi 100 && python main.py -n pinn -l ag -i 10 -ntr 200 -gi 2000 && python main.py -n pinn -l l2 -i 10000 -ntr 3200 -gi 100
+python main.py -n pinn -l l2 -i 10000 -ntr 20000 -gi 100 && python main.py -n pinn -l ag -i 10000 -ntr 200 -gi 2000 && python main.py -n pinn -l l2 -i 10000 -ntr 3200 -gi 100
--- lib/optimizer.py
+++ lib/optimizer.py
@@ -39,24 +39,47 @@ class Optimizer:
         for key in dict_params:
             self.__setattr__(key, dict_params[key])
 
-    def regenerate_data(self, grads_x):
+    def generate_data_by_gradient(self, grads_x, num_new_train_samples):
         xy_eqn, xy_bnd = self.x_train
         num_train_samples = xy_eqn.shape[0]
+        total_num_train_samples = num_train_samples + num_new_train_samples
+        pdb.set_trace()
         self.x_train[0] = tf.Variable(tf.concat((xy_eqn,
                                                  tf.add(xy_eqn, grads_x[0] * 0.01)), axis=0))
-        new_xy_ub = np.random.rand(num_train_samples // 2, 2)  # top-bottom boundaries
+        new_xy_ub = np.random.rand(num_new_train_samples // 2, 2)  # top-bottom boundaries
         new_xy_ub[..., 1] = np.round(new_xy_ub[..., 1])  # y-position is 0 or 1
-        new_xy_lr = np.random.rand(num_train_samples // 2, 2)  # left-right boundaries
+        new_xy_lr = np.random.rand(num_new_train_samples // 2, 2)  # left-right boundaries
         new_xy_lr[..., 0] = np.round(new_xy_lr[..., 0])  # x-position is 0 or 1
         new_xy_bnd = np.random.permutation(np.concatenate([new_xy_ub, new_xy_lr]))
         self.x_train[1] = tf.Variable(tf.concat((xy_bnd, tf.constant(new_xy_bnd, dtype=tf.float32)), axis=0))
 
-        num_train_samples *= 2
         u0 = 1
         # create training output
-        zeros = np.zeros((num_train_samples, 2))
-        uv_bnd = np.zeros((num_train_samples, 2))
+        zeros = np.zeros((total_num_train_samples, 2))
+        uv_bnd = np.zeros((total_num_train_samples, 2))
         uv_bnd[..., 0] = u0 * np.floor(self.x_train[1].numpy()[..., 1])
+        y_train = [zeros, zeros, uv_bnd]
+        self.y_train = [tf.constant(y, dtype=tf.float32) for y in y_train]
+
+    def generate_data_random(self, num_new_train_samples):
+        # inlet flow velocity
+        u0 = 1
+        # create training input
+        xy_eqn = np.random.rand(num_new_train_samples, 2)
+        xy_ub = np.random.rand(num_new_train_samples // 2, 2)  # top-bottom boundaries
+        xy_ub[..., 1] = np.round(xy_ub[..., 1])          # y-position is 0 or 1
+        xy_lr = np.random.rand(num_new_train_samples // 2, 2)  # left-right boundaries
+        xy_lr[..., 0] = np.round(xy_lr[..., 0])          # x-position is 0 or 1
+        xy_bnd = np.random.permutation(np.concatenate([xy_ub, xy_lr]))
+        xy_eqn_old, xy_bnd_old = self.x_train
+        self.x_train[0] = tf.Variable(tf.concat((xy_eqn_old, xy_eqn), axis=0))
+        self.x_train[1] = tf.Variable(tf.concat((xy_bnd_old, xy_bnd), axis=0))
+
+        # create training output
+        total_num_train_samples = self.x_train[0].shape[0]
+        zeros = np.zeros((total_num_train_samples, 2))
+        uv_bnd = np.zeros((total_num_train_samples, 2))
+        uv_bnd[..., 0] = u0 * np.floor(xy_bnd[..., 1])
         y_train = [zeros, zeros, uv_bnd]
         self.y_train = [tf.constant(y, dtype=tf.float32) for y in y_train]
 
@@ -93,7 +116,9 @@ class Optimizer:
                 p = int(1 + 1 / (1.1 - i * 1.0 / self.maxiter))
             loss, grads, grads_x = self.evaluate(self.x_train, self.y_train, p)
             if self.loss == 'ag' and (i + 1) % self.gradient_interval == 0:
-                self.regenerate_data(grads_x)
+                total_num_train_samples = self.x_train[0].shape[0]
+                self.generate_data_by_gradient(grads_x, 100)
+                self.generate_data_random(grads_x, total_num_train_samples - 100)
             optimizer_nn.apply_gradients(zip(grads, self.model.trainable_variables))
             pbar.set_postfix({'loss': '{:.5f}'.format(loss.numpy())})
             pbar.update()
